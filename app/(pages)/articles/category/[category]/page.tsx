cc72ceb1ae8db32c0c3ff95ddb1d2a9e110b96e1
thisara <sasmithajayasinghe1@gmail.com>
2025-01-05 21:43:56 +0530

    ngf

--- app/(pages)/articles/category/[category]/page.tsx
+++ app/(pages)/articles/category/[category]/page.tsx
@@ -4,12 +4,12 @@ import { CATEGORIES_ITEMS } from "@/app/components/Nav/constants";
 import { Article, ArticleCategory } from "@/app/types/article-type";
 import Image from "next/image";
 
+export const dynamic = "force-dynamic"
 export default async function CategoryDefaultPage(p: { params: { category: ArticleCategory; articles : Article[] } }) {
 
     // const categoryItem = CATEGORIES_ITEMS[p.params.category];
     const articles = await ArticleApi.fetchByCategory(p.params.category)
   return (
-
     <div>
       <div className="flex items-center space-x-4 mb-16">
             <Image src={CATEGORIES_ITEMS[p.params.category].src} className="h-10 w-10" alt="Latest News Icon"/>
